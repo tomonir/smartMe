f5024f7632bc4f90f4919e79ed1c814654b2eade
Monir <mohammad.moniruzzaman@valeo.com>
2021-01-07 21:07:24 +0100

    how to run

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #smartMe
+
+How to run:
+python .\main.py -t JobAlert
+python .\main.py -t ContentMonitor
--- smart_me/task.py
+++
@@ -1,8 +0,0 @@
-class Task:
-    def __init__(self,config=None):
-        pass
-    def execute(self):
-        pass
-    def action(self):
-        pass
-    

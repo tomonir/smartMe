5c477538eb6d5b89d6c78419f0144f3dbf280441
Unknown <tomonir@gmail.com>
2021-01-09 11:54:09 +0100

    intial working version

--- smart_me/lib/seleniumBroker.py
+++ smart_me/lib/seleniumBroker.py
@@ -1,42 +1,52 @@
 from lib.logger import Logger
 import importlib
 
+from lib.selenium_parger import SeleniumParger 
+
+
 logger = Logger()
 
 
 class Command(object):
-    def __init__(self,fileds,url):
+    def __init__(self,fileds,selinum_parger):
         self.fields = fileds
-        self.url = url
+        self.selinum_parger = selinum_parger
     def process(self):
         pass
     
 class textFiled(Command):
-    def __init__(self,fields,url):
-        super(self.__class__, self).__init__(fields,url)
+    def __init__(self,fields,selinum_parger):
+        super(self.__class__, self).__init__(fields,selinum_parger)
 
     def process(self):
-        print (self.fields)    
+        #print (self.fields)
+        print (self.fields['filed_name'])
+        print (self.fields['text'])
+        self.selinum_parger.setTextFiled(self.fields['filed_name'],self.fields['text'])     
         
 class search_button(Command):
-    def __init__(self,fields,url):
-        super(self.__class__, self).__init__(fields,url)
+    def __init__(self,fields,selinum_parger):
+        super(self.__class__, self).__init__(fields,selinum_parger)
                     
     def process(self):
-        print (self.fields)
+        print (self.fields['filed_name'])
+        self.selinum_parger.clickOntheButton(self.fields['filed_name'])
           
-class output(Command):
-    def __init__(self,fields,url):
-        super(self.__class__, self).__init__(fields,url)
+class output_area(Command):
+    def __init__(self,fields,selinum_parger):
+        super(self.__class__, self).__init__(fields,selinum_parger)
                     
-    def getOutput(self):
-        return  (self.fields)
+    def process(self):
+        print (self.fields['area_name']+"<<")
+        return self.selinum_parger.getResult(self.fields['area_name'])    
 
+        
 
 
 class SeleniumBroker:
     def __init__(self):
-        pass
+        self.myseleniumParger = SeleniumParger()
+        
     
     def getSearchOutput(self,config):
         #print config
@@ -44,36 +54,38 @@ class SeleniumBroker:
 
         try:
             url=config["url"]
+            self.myseleniumParger.setUrl(url)
         except:
             logger.getLogger(__name__).error('url NOT FOUND!')
             return ""
 
         try:
-            self.__prepareSearchSetup(config["search_setup"],url)
+            self.__prepareSearchSetup(config["search_setup"],self.myseleniumParger)
         except:
             logger.getLogger(__name__).debug('search_setup NOT FOUND!')
             
         try:
-            self.__prepareAction(config["action"],url)
+            print (config["action"])
+            self.__prepareAction(config["action"],self.myseleniumParger)
         except:
             logger.getLogger(__name__).debug('action NOT FOUND!')
             
         try:
-            return self.__prepareOutput(config["search_output"],url)
+            return self.__prepareOutput(config["search_output"],self.myseleniumParger)
         except:
             logger.getLogger(__name__).debug('search_output NOT FOUND!')
             return "" 
                                
                
-    def __prepareSearchSetup(self,config,url):        
+    def __prepareSearchSetup(self,config,selinum_parger):        
         for fileds  in config:
             for filed  in fileds:
                 if (filed=="textFiled"):
-                    textFiled(fileds,url).process()                    
+                    textFiled(fileds[filed],selinum_parger).process()                    
                     
-    def __prepareAction(self,fileds,url):
-        search_button(fileds,url).process()
+    def __prepareAction(self,fileds,selinum_parger):
+            search_button(fileds,selinum_parger).process()
                     
                                                     
-    def __prepareOutput(self,fileds,url):
-        return output(fileds,url).getOutput()    
+    def __prepareOutput(self,fileds,selinum_parger):
+        print (output_area(fileds,selinum_parger).process())    
---
+++ smart_me/lib/selenium_parger.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from lib.logger import Logger
+logger = Logger()
+
+
+
+class SeleniumParger:
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+        #pass
+
+    def setUrl(self,url):
+        self.url    = url
+        self.driver.get(self.url)
+
+    def setTextFiled(self,txt,_xpath):
+        try:
+            element = self.driver.find_element_by_xpath(_xpath)
+            element.clear()
+            element.send_keys(txt)
+        except NoSuchElementException:
+            logger.getLogger(__name__).debug( _xpath + ' Not found !')
+            return    
+        except:
+            logger.getLogger(__name__).debug('setTextFiled Failed!')
+            return
+
+        
+   
+    def clickOntheButton(self,_xpath):
+        try:
+            element = self.driver.find_element_by_xpath(_xpath)
+            element.click()
+        except NoSuchElementException:
+            logger.getLogger(__name__).debug('Button Not found !')
+            return
+        except:
+            logger.getLogger(__name__).debug('clickOntheButton Failed !')
+            return
+
+    def getResult(self,_xpath):
+        try:
+            element = self.driver.find_element_by_xpath(_xpath)
+            return (element.text)
+        except NoSuchElementException:
+            logger.getLogger(__name__).debug('Button Not found !')
+            return ""
+        except:
+            logger.getLogger(__name__).debug('clickOntheButton Failed !')
+            return ""
+
+
+
+
+    
